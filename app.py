f6e0b2128ec4747d320e01ffa27820382c08521c
ciprianivin87-svg <ciprianivin87@gmail.com>
2026-08-29 19:21:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -23,40 +23,8 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-# 1. DATABASE METRICHE SQUADRE E GIOCATORI CHIAVE (SCHEDE TECNICHE COMPATTE)
+# DATABASE METRICHE SQUADRE SERIE A 2026/2027 (AGGIORNATO)
 dati_squadre = {
-    'Inter': {
-        'xG_fatti': 2.05, 'xGA_subiti': 0.80, 'possesso': 58.4,
-        'stelle': [
-            {'nome': 'Lautaro Martinez', 'ruolo': 'ATT', 'stat': 'xG/90: 0.65 | Tiri/90: 3.4'},
-            {'nome': 'Hakan Calhanoglu', 'ruolo': 'CEN', 'stat': 'Passaggi Chiave: 2.8 | Rigori: 95%'},
-            {'nome': 'Nicolò Barella', 'ruolo': 'CEN', 'stat': 'Recuperi: 5.2 | Assist Attesi: 0.25'}
-        ]
-    },
-    'Juventus': {
-        'xG_fatti': 1.70, 'xGA_subiti': 0.85, 'possesso': 52.8,
-        'stelle': [
-            {'nome': 'Dusan Vlahovic', 'ruolo': 'ATT', 'stat': 'xG/90: 0.58 | Conversione: 18%'},
-            {'nome': 'Kenan Yildiz', 'ruolo': 'TRQ', 'stat': 'Dribbling Riusciti: 2.3/90'},
-            {'nome': 'Gleison Bremer', 'ruolo': 'DIF', 'stat': 'Duelli Vinti: 68% | Contrasti: 3.1'}
-        ]
-    },
-    'Milan': {
-        'xG_fatti': 1.85, 'xGA_subiti': 1.10, 'possesso': 54.2,
-        'stelle': [
-            {'nome': 'Christian Pulisic', 'ruolo': 'TRQ', 'stat': 'Partecipazione Gol: 42%'},
-            {'nome': 'Rafael Leao', 'ruolo': 'ATT', 'stat': 'Dribbling: 3.5/90 | Assist Attesi: 0.30'},
-            {'nome': 'Tijjani Reijnders', 'ruolo': 'CEN', 'stat': 'Accuratezza Passaggi: 91%'}
-        ]
-    },
-    'Napoli': {
-        'xG_fatti': 1.75, 'xGA_subiti': 0.95, 'possesso': 56.1,
-        'stelle': [
-            {'nome': 'Romelu Lukaku', 'ruolo': 'ATT', 'stat': 'xG+xA/90: 0.72 | Sponde: 4.8'},
-            {'nome': 'Khvicha Kvaratskhelia', 'ruolo': 'ATT', 'stat': 'Tiri in Porta: 1.8/90'},
-            {'nome': 'Stanislav Lobotka', 'ruolo': 'CEN', 'stat': 'Palloni Recuperati: 6.4/90'}
-        ]
-    },
     'Atalanta': {
         'xG_fatti': 1.90, 'xGA_subiti': 1.15, 'possesso': 53.5,
         'stelle': [
@@ -65,39 +33,143 @@ dati_squadre = {
             {'nome': 'Charles De Ketelaere', 'ruolo': 'TRQ', 'stat': 'Assist Attesi: 0.28/90'}
         ]
     },
-    'Roma': {
-        'xG_fatti': 1.55, 'xGA_subiti': 1.05, 'possesso': 51.0,
+    'Bologna': {
+        'xG_fatti': 1.35, 'xGA_subiti': 1.10, 'possesso': 53.0,
         'stelle': [
-            {'nome': 'Paulo Dybala', 'ruolo': 'TRQ', 'stat': 'xA/90: 0.35 | Tiri da Fuori: 1.4'},
-            {'nome': 'Artem Dovbyk', 'ruolo': 'ATT', 'stat': 'xG/90: 0.52 | Protezione Palla: 78%'}
+            {'nome': 'Riccardo Orsolini', 'ruolo': 'ATT', 'stat': 'xG/90: 0.42 | Tiri/90: 2.9'},
+            {'nome': 'Santiago Castro', 'ruolo': 'ATT', 'stat': 'Pressioni Alte: 11.4/90'}
         ]
     },
-    'Lazio': {
-        'xG_fatti': 1.50, 'xGA_subiti': 1.15, 'possesso': 50.5,
+    'Cagliari': {
+        'xG_fatti': 1.00, 'xGA_subiti': 1.40, 'possesso': 43.5,
         'stelle': [
-            {'nome': 'Taty Castellanos', 'ruolo': 'ATT', 'stat': 'xG/90: 0.48 | Pressioni Alte: 12.3'},
-            {'nome': 'Mattia Zaccagni', 'ruolo': 'ATT', 'stat': 'Falli Subiti: 2.8/90 | Cross: 3.2'}
+            {'nome': 'Roberto Piccoli', 'ruolo': 'ATT', 'stat': 'xG/90: 0.32 | Duelli: 4.1'},
+            {'nome': 'Zito Luvumbo', 'ruolo': 'ATT', 'stat': 'Dribbling: 2.1/90'}
+        ]
+    },
+    'Como': {
+        'xG_fatti': 1.45, 'xGA_subiti': 1.25, 'possesso': 52.0,
+        'stelle': [
+            {'nome': 'Nico Paz', 'ruolo': 'TRQ', 'stat': 'Tiri/90: 2.8 | Assist Attesi: 0.32'},
+            {'nome': 'Patrick Cutrone', 'ruolo': 'ATT', 'stat': 'xG/90: 0.45'}
         ]
     },
     'Fiorentina': {
         'xG_fatti': 1.45, 'xGA_subiti': 1.20, 'possesso': 52.0,
         'stelle': [
-            {'nome': 'Moise Kean', 'ruolo': 'ATT', 'stat': 'xG/90: 0.50 | Scatti in Profondità: 8.5'},
+            {'nome': 'Moise Kean', 'ruolo': 'ATT', 'stat': 'xG/90: 0.50 | Scatti: 8.5'},
             {'nome': 'Albert Gudmundsson', 'ruolo': 'TRQ', 'stat': 'Passaggi Chiave: 2.4/90'}
         ]
     },
-    'Bologna': {'xG_fatti': 1.35, 'xGA_subiti': 1.10, 'possesso': 53.0, 'stelle': [{'nome': 'Riccardo Orsolini', 'ruolo': 'ATT', 'stat': 'xG/90: 0.42 | Tiri/90: 2.9'}]},
-    'Torino': {'xG_fatti': 1.15, 'xGA_subiti': 1.10, 'possesso': 47.5, 'stelle': [{'nome': 'Samuele Ricci', 'ruolo': 'CEN', 'stat': 'Accuratezza Passaggi: 89%'}]},
-    'Genoa': {'xG_fatti': 1.10, 'xGA_subiti': 1.25, 'possesso': 45.0, 'stelle': [{'nome': 'Andrea Pinamonti', 'ruolo': 'ATT', 'stat': 'xG/90: 0.38'}]},
-    'Monza': {'xG_fatti': 1.10, 'xGA_subiti': 1.35, 'possesso': 46.2, 'stelle': [{'nome': 'Daniel Maldini', 'ruolo': 'TRQ', 'stat': 'Tiri in Porta: 1.2/90'}]},
-    'Udinese': {'xG_fatti': 1.15, 'xGA_subiti': 1.30, 'possesso': 44.8, 'stelle': [{'nome': 'Lorenzo Lucca', 'ruolo': 'ATT', 'stat': 'Duelli Aerei Vinti: 4.2/90'}]},
-    'Cagliari': {'xG_fatti': 1.00, 'xGA_subiti': 1.40, 'possesso': 43.5, 'stelle': [{'nome': 'Roberto Piccoli', 'ruolo': 'ATT', 'stat': 'xG/90: 0.32'}]},
-    'Parma': {'xG_fatti': 1.25, 'xGA_subiti': 1.45, 'possesso': 47.0, 'stelle': [{'nome': 'Dennis Man', 'ruolo': 'ATT', 'stat': 'Dribbling: 2.8/90 | xG: 0.39'}]},
-    'Lecce': {'xG_fatti': 0.95, 'xGA_subiti': 1.40, 'possesso': 42.0, 'stelle': [{'nome': 'Nikola Krstovic', 'ruolo': 'ATT', 'stat': 'Tiri Totali: 3.2/90'}]},
-    'Verona': {'xG_fatti': 1.00, 'xGA_subiti': 1.45, 'possesso': 41.5, 'stelle': [{'nome': 'Casper Tengstedt', 'ruolo': 'ATT', 'stat': 'Conversione: 22%'}]},
-    'Empoli': {'xG_fatti': 0.90, 'xGA_subiti': 1.40, 'possesso': 43.0, 'stelle': [{'nome': 'Sebastiano Esposito', 'ruolo': 'ATT', 'stat': 'Occasioni Create: 1.8/90'}]},
-    'Como': {'xG_fatti': 1.10, 'xGA_subiti': 1.50, 'possesso': 48.0, 'stelle': [{'nome': 'Nico Paz', 'ruolo': 'TRQ', 'stat': 'Tiri/90: 2.8 | Assist Attesi: 0.22'}]},
-    'Venezia': {'xG_fatti': 0.95, 'xGA_subiti': 1.75, 'possesso': 41.0, 'stelle': [{'nome': 'Joel Pohjanpalo', 'ruolo': 'ATT', 'stat': 'xG/90: 0.40'}]}
+    'Frosinone': {
+        'xG_fatti': 1.05, 'xGA_subiti': 1.50, 'possesso': 44.0,
+        'stelle': [
+            {'nome': 'Giuseppe Ambrosino', 'ruolo': 'ATT', 'stat': 'xG/90: 0.35'},
+            {'nome': 'Luca Garritano', 'ruolo': 'CEN', 'stat': 'Passaggi Chiave: 1.9/90'}
+        ]
+    },
+    'Genoa': {
+        'xG_fatti': 1.10, 'xGA_subiti': 1.25, 'possesso': 45.0,
+        'stelle': [
+            {'nome': 'Andrea Pinamonti', 'ruolo': 'ATT', 'stat': 'xG/90: 0.38'},
+            {'nome': 'Morten Frendrup', 'ruolo': 'CEN', 'stat': 'Contrasti Vinti: 3.4/90'}
+        ]
+    },
+    'Inter': {
+        'xG_fatti': 2.10, 'xGA_subiti': 0.80, 'possesso': 58.4,
+        'stelle': [
+            {'nome': 'Lautaro Martinez', 'ruolo': 'ATT', 'stat': 'xG/90: 0.65 | Tiri/90: 3.4'},
+            {'nome': 'Hakan Calhanoglu', 'ruolo': 'CEN', 'stat': 'Passaggi Chiave: 2.8'},
+            {'nome': 'Nicolò Barella', 'ruolo': 'CEN', 'stat': 'Recuperi: 5.2/90'}
+        ]
+    },
+    'Juventus': {
+        'xG_fatti': 1.70, 'xGA_subiti': 0.85, 'possesso': 53.0,
+        'stelle': [
+            {'nome': 'Dusan Vlahovic', 'ruolo': 'ATT', 'stat': 'xG/90: 0.58 | Conversione: 18%'},
+            {'nome': 'Kenan Yildiz', 'ruolo': 'TRQ', 'stat': 'Dribbling: 2.3/90'},
+            {'nome': 'Gleison Bremer', 'ruolo': 'DIF', 'stat': 'Duelli Vinti: 68%'}
+        ]
+    },
+    'Lazio': {
+        'xG_fatti': 1.50, 'xGA_subiti': 1.15, 'possesso': 50.5,
+        'stelle': [
+            {'nome': 'Taty Castellanos', 'ruolo': 'ATT', 'stat': 'xG/90: 0.48'},
+            {'nome': 'Mattia Zaccagni', 'ruolo': 'ATT', 'stat': 'Falli Subiti: 2.8/90'}
+        ]
+    },
+    'Lecce': {
+        'xG_fatti': 0.95, 'xGA_subiti': 1.40, 'possesso': 42.0,
+        'stelle': [
+            {'nome': 'Nikola Krstovic', 'ruolo': 'ATT', 'stat': 'Tiri Totali: 3.2/90'},
+            {'nome': 'Lameck Banda', 'ruolo': 'ATT', 'stat': 'Dribbling: 2.4/90'}
+        ]
+    },
+    'Milan': {
+        'xG_fatti': 1.85, 'xGA_subiti': 1.10, 'possesso': 54.2,
+        'stelle': [
+            {'nome': 'Christian Pulisic', 'ruolo': 'TRQ', 'stat': 'Partecipazione Gol: 42%'},
+            {'nome': 'Rafael Leao', 'ruolo': 'ATT', 'stat': 'Dribbling: 3.5/90'},
+            {'nome': 'Tijjani Reijnders', 'ruolo': 'CEN', 'stat': 'Passaggi: 91%'}
+        ]
+    },
+    'Monza': {
+        'xG_fatti': 1.10, 'xGA_subiti': 1.35, 'possesso': 46.2,
+        'stelle': [
+            {'nome': 'Milan Djuric', 'ruolo': 'ATT', 'stat': 'Duelli Aerei: 5.8/90'},
+            {'nome': 'Matteo Pessina', 'ruolo': 'CEN', 'stat': 'Accuratezza Passaggi: 88%'}
+        ]
+    },
+    'Napoli': {
+        'xG_fatti': 1.75, 'xGA_subiti': 0.95, 'possesso': 56.1,
+        'stelle': [
+            {'nome': 'Romelu Lukaku', 'ruolo': 'ATT', 'stat': 'xG+xA/90: 0.72'},
+            {'nome': 'Khvicha Kvaratskhelia', 'ruolo': 'ATT', 'stat': 'Tiri in Porta: 1.8/90'},
+            {'nome': 'Stanislav Lobotka', 'ruolo': 'CEN', 'stat': 'Recuperi: 6.4/90'}
+        ]
+    },
+    'Parma': {
+        'xG_fatti': 1.25, 'xGA_subiti': 1.45, 'possesso': 47.0,
+        'stelle': [
+            {'nome': 'Dennis Man', 'ruolo': 'ATT', 'stat': 'Dribbling: 2.8/90 | xG: 0.39'},
+            {'nome': 'Ange-Yoan Bonny', 'ruolo': 'ATT', 'stat': 'Sponde Chiave: 2.1/90'}
+        ]
+    },
+    'Roma': {
+        'xG_fatti': 1.60, 'xGA_subiti': 1.00, 'possesso': 52.5,
+        'stelle': [
+            {'nome': 'Paulo Dybala', 'ruolo': 'TRQ', 'stat': 'xA/90: 0.35 | Tiri: 2.2'},
+            {'nome': 'Artem Dovbyk', 'ruolo': 'ATT', 'stat': 'xG/90: 0.54'}
+        ]
+    },
+    'Sassuolo': {
+        'xG_fatti': 1.20, 'xGA_subiti': 1.45, 'possesso': 48.5,
+        'stelle': [
+            {'nome': 'Domenico Berardi', 'ruolo': 'ATT', 'stat': 'xG+xA/90: 0.68'},
+            {'nome': 'Armand Laurienté', 'ruolo': 'ATT', 'stat': 'Dribbling: 2.9/90'}
+        ]
+    },
+    'Torino': {
+        'xG_fatti': 1.15, 'xGA_subiti': 1.10, 'possesso': 47.5,
+        'stelle': [
+            {'nome': 'Duvan Zapata', 'ruolo': 'ATT', 'stat': 'xG/90: 0.44'},
+            {'nome': 'Samuele Ricci', 'ruolo': 'CEN', 'stat': 'Passaggi: 89%'}
+        ]
+    },
+    'Udinese': {
+        'xG_fatti': 1.15, 'xGA_subiti': 1.30, 'possesso': 44.8,
+        'stelle': [
+            {'nome': 'Lorenzo Lucca', 'ruolo': 'ATT', 'stat': 'Duelli Aerei: 4.2/90'},
+            {'nome': 'Florian Thauvin', 'ruolo': 'TRQ', 'stat': 'Passaggi Chiave: 2.1/90'}
+        ]
+    },
+    'Venezia': {
+        'xG_fatti': 0.95, 'xGA_subiti': 1.70, 'possesso': 42.0,
+        'stelle': [
+            {'nome': 'Joel Pohjanpalo', 'ruolo': 'ATT', 'stat': 'xG/90: 0.40'},
+            {'nome': 'Gianluca Busio', 'ruolo': 'CEN', 'stat': 'Inserimenti/90: 3.1'}
+        ]
+    }
 }
 
 squadre = sorted(list(dati_squadre.keys()))
